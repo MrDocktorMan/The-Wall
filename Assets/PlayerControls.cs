feac7e14bd265252591e143a0df87488260fc933
MrDocktorMan <mrdocktorman@gmail.com>
2024-02-17 16:06:40 -0500

    Updated Player Controls

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerControls : MonoBehaviour
 {
@@ -11,7 +13,14 @@ public class PlayerControls : MonoBehaviour
     public float speed;
     public float jumpForce;
     public float jumpTimer;
+    bool isGrounded;
+    public float GroundHeight;
+    public float collRadius;
+    public LayerMask GroundTouchy;
+    Vector2 sphereLocation;
 
+    public float springPower;
+    public bool onSpring = false;
 
 
     // Start is called before the first frame update
@@ -23,12 +32,15 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-        
+
+
+        sphereLocation = new Vector2(transform.position.x, transform.position.y - GroundHeight);
+        isGrounded = Physics2D.OverlapCircle(sphereLocation,collRadius,GroundTouchy);
+        print(isGrounded);
 
         
 
-       if(Input.GetButtonDown("Jump"))
+       if(Input.GetButtonDown("Jump") && isGrounded == true && airTime <= 0)
         {
             airTime = jumpTimer;
         }
@@ -47,16 +59,68 @@ public class PlayerControls : MonoBehaviour
     {
         velocity.x = Input.GetAxisRaw("Horizontal") * speed;
        
-        print(airTime);
+        //print(airTime);
         if (airTime > 0)
         {
+
+
             airTime -= Time.deltaTime;
             rb.velocity = Vector2.up * jumpForce;
         }
-        //
+        
+
+        if (!onSpring)
+        {
+            rb.velocity = new Vector2(velocity.x, rb.velocity.y);
+        }
+        else
+        {
+            Invoke("onSpringOff", 0.2f);
+        }
+
 
 
         rb.velocity = new Vector2(velocity.x,rb.velocity.y);
     
     }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(sphereLocation, collRadius);
+
+    }
+
+
+    void onSpringOff()
+    {
+        onSpring = false;
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "wall")
+        {
+            print("wall");
+            SceneManager.LoadScene("GameScene");
+        }
+        if (col.gameObject.tag == "levelEnd")
+        {
+            SceneManager.LoadScene("winScene");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "spring")
+        {
+            onSpring = true;
+            print("spring");
+            rb.AddForce(new Vector2(-springPower, 0f), ForceMode2D.Impulse);
+        }
+    }
+
+
+
+
+
 }
